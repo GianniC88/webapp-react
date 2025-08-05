923f392135ce1768bf32fde23666768fb26928ab
GianniC88 <gianni.colucci.88@gmail.com>
2025-08-05 19:19:13 +0200

    feat: add NotFound component and update routing in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-
-
 import { BrowserRouter, Router, Route, Routes } from 'react-router-dom'
 import DefaultLayout from './assets/layout/DefaultLayout'
 import HomePage from './assets/pages/HomePage'
 import SingleMovies from './assets/pages/SingleMoviesPage'
 import MoviesPage from './assets/pages/moviesPage'
+import NotFound from './assets/components/NotFound'
 function App() {
 
 
@@ -17,6 +16,7 @@ function App() {
             <Route path='/homepage' element={<HomePage />} />
             <Route path='/movies' element={<MoviesPage />} />
             <Route path='/movies/:id' element={<SingleMovies />} />
+            <Route path='*' element={<NotFound />} />
           </Route>
         </Routes>
       </BrowserRouter>
